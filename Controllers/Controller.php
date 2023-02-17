b1f0570178216fa3abb360bcb0c516970366c162
TatsukiUchiyama <fukunaga.kinoko4929@gmail.com>
2023-02-17 14:27:24 +0900

    protectedのため編集用の関数を使用

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -5,10 +5,15 @@ class Controller
     public function view(string $template, array $params = []): void
     {
         $Smarty = new Smarty();
-        $Smarty->template_dir = ROOT_PATH.'Views';
-        $Smarty->compile_dir  = ROOT_PATH.'Views/compile';
+        $Smarty->setTemplateDir(ROOT_PATH.'Views');
+        $Smarty->setCompileDir(ROOT_PATH.'Views/compile');
         $Smarty->escape_html = true;
         $Smarty->assign($params);
-        $Smarty->display($template.'.tpl');
+        try {
+            $Smarty->display($template . '.tpl');
+        } catch (SmartyException|Exception $e) {
+            echo $e->getMessage();
+            exit();
+        }
     }
 }
